4774b46c454565541e7eb46462d7f537d7ea8b74
Ana Atuesta <gabrielaatuesta@gmail.com>
2024-08-15 15:31:55 -0500

    Cubit de detalle personaje corregido

--- test/cubits/detalle_personaje_cubit_test.dart
+++ test/cubits/detalle_personaje_cubit_test.dart
@@ -2,6 +2,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_rick_and_morty/domain/entities/character.dart';
 import 'package:flutter_rick_and_morty/presentation/cubits/detalle_personaje_cubit.dart';
+import 'package:flutter_rick_and_morty/presentation/cubits/detalle_personaje_state.dart';
 
 void main() {
   late DetallePersonajeCubit detallePersonajeCubit;
@@ -25,6 +26,23 @@ void main() {
     'emits [DetallePersonajeLoading, DetallePersonajeLoaded] when loadCharacterDetail is called',
     build: () => detallePersonajeCubit,
     act: (cubit) => cubit.loadCharacterDetail(character),
-    expect: () => [DetallePersonajeLoading(), DetallePersonajeLoaded(character)],
+    expect: () => [
+      DetallePersonajeLoading(),
+      isA<DetallePersonajeLoaded>().having((state) => state.character.name, 'name', 'Rick Sanchez'),
+    ],
+  );
+
+  blocTest<DetallePersonajeCubit, DetallePersonajeState>(
+    'emits [DetallePersonajeLoading, DetallePersonajeError] when loadCharacterDetail fails',
+    build: () => detallePersonajeCubit,
+    act: (cubit) {
+      // Simular un error arrojando una excepción dentro del método
+      cubit.loadCharacterDetail(character);
+      throw Exception('Error loading character details');
+    },
+    expect: () => [
+      DetallePersonajeLoading(),
+      isA<DetallePersonajeError>(),
+    ],
   );
 }
